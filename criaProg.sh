ff15cf43cd8e443b4b636706ae15117a3c55f604
lorenzomoulin <lorenzomoulin@gmail.com>
2018-03-26 18:49:43 +0000

    Add files via upload

---
+++ criaProg.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+COMANDO=$(command -v xdotool)
+
+if [ "$COMANDO" ];then
+	gedit $1 &
+	sleep 2.5
+	xdotool type '#include<stdio.h>'
+	xdotool key Return; xdotool key Return; xdotool type 'int main(){';xdotool key Return
+	xdotool key Tab; xdotool type 'return 0;'; xdotool key Return; xdotool type '}'
+else 
+	dialog --title "Erro" --infobox "Xdotool nao instalado" 10 30; sleep 4; clear
+fi 
